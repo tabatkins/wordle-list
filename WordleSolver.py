06c907050a0df3d5b57e11849efe04ba2ff5e822
bdp1312 <bdp1312@gmail.com>
2022-03-13 23:29:20 -0500

    started python script. added gitignore

---
+++ WordleSolver.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import sys
+import os
+
+#full file name
+wordFilePath = os.path.expanduser("~/Wordle-Solver_bdp1312/.words.txt")
+
+
+#open file
+
+
+#add each word to list
+
+#while there is more than 1 possible word:
+
+#analize letter frequency
+
+#sort words by sum of letter frequency
+
+#select word with greatest score
+
+#get updated word information from guess
+
+
+
